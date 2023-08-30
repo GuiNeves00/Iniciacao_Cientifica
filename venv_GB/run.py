bcbf2a595c136ceed870f7b308fda022108eb287
Guilherme Neves <86065464+GuiNeves00@users.noreply.github.com>
2023-08-30 16:37:16 -0300

    download da base de dados adiciondo

--- venv_GB/run.py
+++ venv_GB/run.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, request, render_template, url_for, jsonify
+from flask import Flask, redirect, request, render_template, url_for, jsonify, make_response
 from app import geoparsing
 from database.database import Database
 from pathlib import Path
@@ -49,62 +49,6 @@ def evaluate():
         
     return render_template('evaluate.html', texto=txt_exibir, toponimos=toponimos, id_noticia_avaliada=noticia_avaliar)
 
-# def load_existing_data():
-#     if temp_path.is_file() and temp_path.stat().st_size > 0:
-#         with open(temp_path, 'r', encoding='utf-8') as arq:
-#             existing_data = json.load(arq)
-#         return existing_data
-#     else:
-#         return []
-
-# @app.route('/submit-form', methods=['POST'])
-# def submit_form():
-#     dados = request.get_json()
-#     print("******************")
-#     print(dados)
-#     print("******************")
-    
-#     existing_data = load_existing_data()  # Carrega os dados existentes do arquivo JSON
-    
-#     for resposta in dados:
-#         if "palavra" in resposta:
-#             resposta_renomeada = OrderedDict()
-#             resposta_renomeada["palavra"] = resposta["palavra"]
-            
-#             if "is_toponimo" in resposta:
-#                 if isinstance(resposta["is_toponimo"], str):
-#                     resposta_renomeada["is_toponimo"] = resposta["is_toponimo"].lower() == "sim"
-#                 else:
-#                     resposta_renomeada["is_toponimo"] = resposta["is_toponimo"]
-            
-#             if "tipo" in resposta:
-#                 resposta_renomeada["tipo"] = resposta["tipo"] if resposta["tipo"] != "" else None
-            
-#             if "localizacao" in resposta:
-#                 resposta_renomeada["localizacao"] = resposta["localizacao"] if resposta["localizacao"] != "" else None
-            
-#             if not resposta_renomeada.get("is_toponimo", False):
-#                 resposta_renomeada["tipo"] = None
-#                 resposta_renomeada["localizacao"] = None
-            
-#             # Verifica se a resposta já existe nos dados existentes antes de adicionar
-#             if resposta_renomeada not in existing_data:
-#                 existing_data.append(resposta_renomeada)
-    
-#     with open(temp_path, 'w', encoding='utf-8') as arq:
-#         json.dump(existing_data, arq, ensure_ascii=False, indent=4)
-    
-#     return 'Respostas Salvas com Sucesso!'
-
-
-# @app.route('/load-data', methods=['GET'])
-# def load_data():
-#     # Carrega os dados existentes do arquivo JSON
-#     existing_data = load_existing_data()
-
-#     # Retorna os dados existentes como resposta à solicitação GET
-#     return jsonify(existing_data)
-
 @app.route('/obrigado')
 def obrigado():
     return render_template('obrigado.html')
@@ -122,9 +66,18 @@ def processamento():
 
     return redirect(url_for('evaluate'))
 
-@app.route('/downloads')
+@app.route('/downloads', methods=['GET'])
 def downloads():
-    return render_template('downloads.html')
+    try:
+        with open(db_path, 'rb') as file:
+            file_content = file.read()
+        
+        response = make_response(file_content)
+        response.headers['Content-Disposition'] = 'attachment; filename=db.json'
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    except Exception as e:
+        return jsonify({'error':str(e)})
 
 @app.route('/salvar_json', methods=['POST'])
 def salvar_json():
